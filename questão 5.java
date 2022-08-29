c71f478eb844d0103fa9e978ca7ec55ea787f95b
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-08-29 10:53:48 -0300

    Create questão 5.java

---
+++ questão 5.java
@@ -0,0 +1,12 @@
+package Lista2;
+
+public class questão5 {
+/* 5) Imprima a saída: {0,3,6,9 ...99} */
+	public static void main(String[] args) {
+		int k;
+		for(k=0;k<=99;k+=3) {
+			System.out.print(k + " - ");
+		}
+	}
+
+}
