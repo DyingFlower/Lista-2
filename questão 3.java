da3cecaccd1b09602166b24db43b94c891b9ef09
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-08-29 10:51:05 -0300

    Create questão 3.java

---
+++ questão 3.java
@@ -0,0 +1,13 @@
+package Lista2;
+
+public class questão3 {
+/* 3) Imprima a saída: {100, 99, 98, ... 1} */
+	public static void main(String[] args) {
+		int x;
+		for(x=100;x>=1;x--) {
+			System.out.print(x + " - ");
+		}
+
+	}
+
+}
