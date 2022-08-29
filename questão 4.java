389f23c4a7f6bbef43716b98f403656d4edd0a75
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-08-29 10:53:25 -0300

    Create questão 4.java

---
+++ questão 4.java
@@ -0,0 +1,12 @@
+package Lista2;
+
+public class questão4 {
+/* 4) Imprima a saída: {0,2,4,6,8 ... 100} */
+	public static void main(String[] args) {
+		int j;
+		for(j=0;j<=100;j+=2) {
+			System.out.print(j + " - ");
+		}
+	}
+
+}
