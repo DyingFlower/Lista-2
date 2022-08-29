5f8050aa55193208b5127899ebcaaa59abdc8d03
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-08-29 10:50:32 -0300

    Create questão 2.java

---
+++ questão 2.java
@@ -0,0 +1,13 @@
+package Lista2;
+
+public class questão2 {
+/* 2) Imprima a saída: {1,2,3, ... 100} */
+	public static void main(String[] args) {
+		int A;
+		for(A=1;A<=100;A++) {
+			System.out.print(A + " - ");
+		}
+
+	}
+
+}
