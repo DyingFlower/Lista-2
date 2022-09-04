7ad2d6f98d0defffde18022a6e34cbffbe49d7f9
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-09-04 15:46:02 -0300

    Update questão 2.java

--- questão 2.java
+++ questão 2.java
@@ -1,6 +1,6 @@
 package Lista2;
 
-public class questão2 {
+public class questão2 { // as questões a seguir mostram que a impressão tem vírgula, então coloquei vírgulas, mesmo não seguindo o formato que coloca "-".
 /* 2) Imprima a saída: {1,2,3, ... 100} */
 	public static void main(String[] args) {
 		int A;
